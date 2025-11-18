cfca8b2a79bb7419231687f464ae72e00230463f
Kamlesh Kumar Singh <42679581+KamleshSingh7461@users.noreply.github.com>
2025-11-18 22:41:42 +0530

    Update vite.config.ts
    vite updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for better compatibility
+  base: '/drnick/', // Use relative paths for better compatibility
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
@@ -52,7 +52,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-    outDir: 'build',
+    outDir: 'dist',
   },
   server: {
     port: 3000,
